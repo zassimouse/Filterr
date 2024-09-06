82f2581691d31ca690f4d79fb9bb8e3b292af1e8
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2024-09-07 00:59:54 +0300

    Initial Commit

---
+++ Filterr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Filterr
+//
+//  Created by Denis Haritonenko on 7.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Filterr/FilterrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterrApp.swift
+//  Filterr
+//
+//  Created by Denis Haritonenko on 7.09.24.
+//
+
+import SwiftUI
+
+@main
+struct FilterrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
