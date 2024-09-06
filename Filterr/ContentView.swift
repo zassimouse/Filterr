6251e90b89b9a34012bd8e1087491388a04376ce
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2024-09-07 01:14:36 +0300

    Initial commit

--- Filterr/ContentView.swift
+++ Filterr/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var intensity: Float = 50
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+                Slider(value: $intensity)
+            }
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "plus")
+                    })
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Save") {}
+                }
+            }
         }
-        .padding()
     }
 }
 
